ac75333f75515337a506f2968e79ed97476603e6
Megumu Niihara <nhrmgm1223@gmail.com>
2022-05-23 14:37:50 +0900

    fix : format.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <p>init index page.</p>;
-};
+  return <p>init index page.</p>
+}
 
-export default Home;
+export default Home
