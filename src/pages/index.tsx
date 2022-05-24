d01b0951f04232777bb25c7eddddaf3b49042382
Megumu Niihara <nhrmgm1223@gmail.com>
2022-05-24 16:01:03 +0900

    add: Show logo at inital loading.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,20 @@
-import { Button, Container, useColorMode } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 
+import Logo from '@/components/atoms/Logo'
+import FullScreenCenter from '@/components/template/FullScreenCenter'
+import { useInitalLoading } from '@/hooks/useInitalLoading'
+
 const Home: NextPage = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
-  return (
-    <Container maxW="container.xl" my={4}>
-      <Button
-        bg="sTButton"
-        border="1px solid"
-        borderColor="sTSecondColor"
-        color="sTParagraph"
-        onClick={toggleColorMode}
-      >
-        {colorMode}
-      </Button>
-    </Container>
-  )
+  const isLoading = useInitalLoading(2000)
+
+  if (isLoading) {
+    return (
+      <FullScreenCenter>
+        <Logo />
+      </FullScreenCenter>
+    )
+  }
+  return <p>初回ロゴ表示完了</p>
 }
 
 export default Home
