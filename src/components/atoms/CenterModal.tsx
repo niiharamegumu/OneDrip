9a912b897af355a561fa4f273b86074f92141d0c
Megumu Niihara <nhrmgm1223@gmail.com>
2022-05-25 18:44:48 +0900

    add: center modal componetn.

---
+++ src/components/atoms/CenterModal.tsx
@@ -0,0 +1,31 @@
+import { Modal, ModalBody, ModalContent, ModalOverlay } from '@chakra-ui/react'
+import { FC, memo, ReactNode } from 'react'
+
+type Props = {
+  onClose: () => void
+  isOpen: boolean
+  children: ReactNode
+}
+
+// eslint-disable-next-line react/display-name
+const CenterModal: FC<Props> = memo((props) => {
+  const { onClose, isOpen, children } = props
+  return (
+    <Modal onClose={onClose} isOpen={isOpen} isCentered>
+      <ModalOverlay />
+      <ModalContent
+        maxW="min(calc( 100% - 60px ), 400px)"
+        bg="sTButton"
+        color="sTHeading"
+        border="1px solid"
+        borderColor="white"
+      >
+        <ModalBody textAlign="center" fontWeight="bold" p="30px">
+          {children}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+})
+
+export default CenterModal
