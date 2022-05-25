3d39698da1192b9a2d5a4e0dae0d8fa22eee00a5
Megumu Niihara <nhrmgm1223@gmail.com>
2022-05-25 09:53:15 +0900

    add : mypage.

---
+++ src/pages/mypage.tsx
@@ -0,0 +1,26 @@
+import { Avatar, Button } from '@chakra-ui/react'
+import { NextPage } from 'next'
+
+import { useAuth } from '@/hooks/useAuth'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
+import { logout } from '@/libs/firebase/auth'
+
+const Sample: NextPage = () => {
+  const { currentUser } = useCurrentUser()
+  useAuth()
+
+  return (
+    <>
+      <p>{currentUser!.displayName}</p>
+      <p>{currentUser!.email}</p>
+      <p>{currentUser!.emailVerified ? 'メール認証済み' : 'メール未認証'}</p>
+      <p>{currentUser!.displayName}</p>
+      <Avatar name={`${currentUser!.displayName}`} src={`${currentUser?.photoURL}`} />
+      <Button type="button" onClick={logout}>
+        ログアウトする
+      </Button>
+    </>
+  )
+}
+
+export default Sample
