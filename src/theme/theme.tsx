37b225126d8154b77ab55bea68feab4361f7faa9
Megumu Niihara <nhrmgm1223@gmail.com>
2022-05-25 19:44:51 +0900

    update : button default style.

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,4 +1,9 @@
-import { extendTheme, type ThemeConfig, type ComponentStyleConfig } from '@chakra-ui/react'
+import {
+  extendTheme,
+  withDefaultProps,
+  type ThemeConfig,
+  type ComponentStyleConfig,
+} from '@chakra-ui/react'
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
@@ -19,38 +24,46 @@ const Heading: ComponentStyleConfig = {
     color: 'sTHeading',
   },
 }
-
-const theme = extendTheme({
-  components: {
-    Heading,
-  },
-  semanticTokens: {
-    colors: {
-      sTMainColor: {
-        _light: '#F9F4EF',
-        _dark: '#020826',
-      },
-      sTSecondColor: {
-        _light: '#8C7851',
-        _dark: '#F9F4EF',
-      },
-      sTHeading: {
-        _light: '#020826',
-        _dark: '#F9F4EF',
-      },
-      sTParagraph: {
-        _light: '#716040',
-        _dark: '#F9F4EF',
-      },
-      sTButton: {
-        _light: '#FFFFFE',
-        _dark: '#020826',
-      },
-      sTNotice: '#EB5757',
+const theme = extendTheme(
+  withDefaultProps({
+    defaultProps: {
+      variant: 'unstyled',
+      size: 'auto',
     },
+    components: ['Button'],
+  }),
+  {
+    components: {
+      Heading,
+    },
+    semanticTokens: {
+      colors: {
+        sTMainColor: {
+          _light: '#F9F4EF',
+          _dark: '#020826',
+        },
+        sTSecondColor: {
+          _light: '#8C7851',
+          _dark: '#F9F4EF',
+        },
+        sTHeading: {
+          _light: '#020826',
+          _dark: '#F9F4EF',
+        },
+        sTParagraph: {
+          _light: '#716040',
+          _dark: '#F9F4EF',
+        },
+        sTButton: {
+          _light: '#FFFFFE',
+          _dark: '#020826',
+        },
+        sTNotice: '#EB5757',
+      },
+    },
+    styles,
+    config,
   },
-  styles,
-  config,
-})
+)
 
 export default theme
