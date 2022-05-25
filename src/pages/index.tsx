317edb303b145aee0dcb6cbb8c7e28ff0bb7e949
Megumu Niihara <nhrmgm1223@gmail.com>
2022-05-25 09:52:56 +0900

    update: top and login page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
+import { Button, Flex } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 import Logo from '@/components/atoms/Logo'
 import FullScreenCenter from '@/components/template/FullScreenCenter'
+import { useAuth } from '@/hooks/useAuth'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
 import { useInitalLoading } from '@/hooks/useInitalLoading'
+import { login } from '@/libs/firebase/auth'
 
 const Home: NextPage = () => {
-  const isLoading = useInitalLoading(2000)
+  const isLoading = useInitalLoading(1500)
+  const isComplete = useAuth()
+  const { isAuth } = useCurrentUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (isAuth) router.push('/mypage')
+  }, [router, isAuth])
 
   if (isLoading) {
     return (
@@ -14,7 +27,29 @@ const Home: NextPage = () => {
       </FullScreenCenter>
     )
   }
-  return <p>初回ロゴ表示完了</p>
+
+  if (!isComplete) {
+    return (
+      <FullScreenCenter>
+        <p>認証中...</p>
+      </FullScreenCenter>
+    )
+  }
+
+  return (
+    <>
+      {!isAuth ? (
+        <FullScreenCenter>
+          <Flex flexDirection="column" gap={6}>
+            <Logo />
+            <Button type="button" onClick={login} bg="white">
+              ログインする
+            </Button>
+          </Flex>
+        </FullScreenCenter>
+      ) : null}
+    </>
+  )
 }
 
 export default Home
