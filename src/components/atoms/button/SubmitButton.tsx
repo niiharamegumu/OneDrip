4d13796190fd061c92eb1499442d8b2b0eadbb07
Megumu Niihara <nhrmgm1223@gmail.com>
2022-05-28 14:21:44 +0900

    add : submit button.

---
+++ src/components/atoms/button/SubmitButton.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@chakra-ui/react'
+import { FC, memo } from 'react'
+
+type Props = {
+  text: string
+  isLoading: boolean
+}
+
+// eslint-disable-next-line react/display-name
+export const SubmitButton: FC<Props> = memo((props) => {
+  const { text, isLoading } = props
+  return (
+    <Button
+      type="submit"
+      color="sTParagraph"
+      border="1px solid"
+      bg="sTButton"
+      borderColor="sTSecondColor"
+      py="20px"
+      w="100%"
+      isLoading={isLoading}
+    >
+      {text}
+    </Button>
+  )
+})
