3a9fa9b5785c3292146a2048e2e3a64bcac4282e
Megumu Niihara <nhrmgm1223@gmail.com>
2022-05-28 09:55:22 +0900

    update : item cart by status.

--- src/components/organisms/ItemCard.tsx
+++ src/components/organisms/ItemCard.tsx
@@ -10,22 +10,25 @@ import { CoffeeItem } from '@/typs/coffeeItem'
 
 type Props = {
   item: CoffeeItem
+  isElapsed?: boolean
 }
 
 const ItemCard: FC<Props> = (props) => {
-  const { item } = props
+  const { item, isElapsed = true } = props
+  const isActice = item.status === 'active'
+  const isComplete = item.status === 'complete'
 
   return (
     <GridItem key={item.id} borderRadius="10px" overflow="hidden" position="relative" bg="sTCard">
       <SkeletonImage url={item.img_url} altText={item.name} />
-      {isWithinPeriodOfTime(new Date(), item.peek.start, item.peek.end) ? (
+      {isWithinPeriodOfTime(new Date(), item.peek.start, item.peek.end) && isActice ? (
         <NoticeBadge status="peek">Peek</NoticeBadge>
       ) : null}
-      {isAfter(new Date(), item.peek.end) ? (
+      {isAfter(new Date(), item.peek.end) && isActice ? (
         <NoticeBadge status="after">Peek After</NoticeBadge>
       ) : null}
       <Box px={4} py={4}>
-        <Box mb={4}>
+        <Box mb={4} hidden={!isElapsed}>
           <HeadWithText
             title={calcElapsedTime(new Date(), item.standardDate)}
             text="経過"
@@ -46,6 +49,7 @@ const ItemCard: FC<Props> = (props) => {
           <Text>焙煎日：{formatDate(item.standardDate)}</Text>
           <Text>ピーク開始：{formatDate(item.peek.start)}</Text>
           <Text>ピーク終了：{formatDate(item.peek.end)}</Text>
+          {item.completedAt && isComplete && <Text>消費日：{formatDate(item.completedAt)}</Text>}
         </Box>
       </Box>
     </GridItem>
